d8123593e82890495453c2eea9dcaad5468faf0f
hugofpaiva <hugofpaiva@ua.pt>
2020-03-11 23:56:17 +0000

    Organização e pequenas correções

--- lab03/Rua/Main.java
+++ lab03/Rua/Main.java
@@ -7,7 +7,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class Main {
 	private static ArrayList<Familia> families = new ArrayList<Familia>(); // LISTA COM TODAS AS FAMÍLIAS
 
@@ -20,7 +19,7 @@ public class Main {
 				String[] command = sc.nextLine().split(" ");
 				run(command);
 			}
-		// NO CASO DE HAVER UM ARGUMENTO (FICHEIRO DE COMANDOS)
+			// NO CASO DE HAVER UM ARGUMENTO (FICHEIRO DE COMANDOS)
 		} else if (args.length == 1) {
 			try {
 				File file = new File("./" + args[0]);
@@ -33,7 +32,7 @@ public class Main {
 				System.err.println("File does not exist!");
 				System.exit(0);
 			}
-		// NO CASO DE HAVEREM MAIS DE 1 ARGUMENTO (CHAMADA INVÁLIDA)
+			// NO CASO DE HAVEREM MAIS DE 1 ARGUMENTO (CHAMADA INVÁLIDA)
 		} else {
 			System.err.println("Entry unknown!");
 			System.exit(0);
@@ -58,7 +57,7 @@ public class Main {
 								// VERIFICA SE COMEÇA PELO INTERVALO OU PELO NOME DA PESSOA
 								if (Character.isLetter(info[0].charAt(0))) {
 									add(info[0], Integer.parseInt(info[1]), Integer.parseInt(info[2]));
-								} else if (Character.isDigit(info[0].charAt(0))){
+								} else if (Character.isDigit(info[0].charAt(0))) {
 									add(info[2], Integer.parseInt(info[0]), Integer.parseInt(info[1]));
 								}
 							}
@@ -78,7 +77,8 @@ public class Main {
 				break;
 			case "add":
 				// VERIFICA SE O COMANDO É CHAMADO CORRETAMENTE
-				if (command.length == 4 && Character.isDigit(command[2].charAt(0)) && Character.isDigit(command[3].charAt(0))) {
+				if (command.length == 4 && Character.isDigit(command[2].charAt(0))
+						&& Character.isDigit(command[3].charAt(0))) {
 					add(command[1], Integer.parseInt(command[2]), Integer.parseInt(command[3]));
 				} else {
 					System.err.println("Options incorrect for add!");
@@ -123,33 +123,35 @@ public class Main {
 
 	// ADICIONA A PESSA PESSOA A UMA FAMILIA (EXISTENTE OU NOVA)
 	private static void add(String name, int pos_in, int pos_fin) {
-		boolean new_family = true; 		// VARIAVEL QUE CONTROLA SE É NECESSÁRIO CRIAR UMA FAMÍLIA NOVA PARA ESTA PESSOA
+		boolean new_family = true; // VARIAVEL QUE CONTROLA SE É NECESSÁRIO CRIAR UMA FAMÍLIA NOVA PARA ESTA PESSOA
 		Pessoa p = new Pessoa(name);
-
-		if (families.isEmpty()) {
-			Familia f = new Familia(p, pos_in, pos_fin);
-			families.add(f);
-			System.out.println("\n" + name + " adicionado(a) com sucesso à familia " + pos_in + "-" + pos_fin);
-		} else {
-			for (Familia f : families) {
-				int status = f.addMembro(p, pos_in, pos_fin);
-				// VERIFICA SE A PESSOA FOI ADICIONADA COM SUCESSO
-				if (status == 0) {
-					new_family = false;
-					break;
-				} else if (status == 2) { // VERIFICA SE O NOME JÁ EXISTE
-					new_family = false;
-					break;
-				}
-			}
-
-
-			if (new_family) {
+		if (pos_fin >= pos_in && pos_in > 0) {
+			if (families.isEmpty()) {
 				Familia f = new Familia(p, pos_in, pos_fin);
 				families.add(f);
 				System.out.println("\n" + name + " adicionado(a) com sucesso à familia " + pos_in + "-" + pos_fin);
+			} else {
+				for (Familia f : families) {
+					int status = f.addMembro(p, pos_in, pos_fin);
+					// VERIFICA SE A PESSOA FOI ADICIONADA COM SUCESSO
+					if (status == 0) {
+						new_family = false;
+						break;
+					} else if (status == 2) { // VERIFICA SE O NOME JÁ EXISTE
+						new_family = false;
+						break;
+					}
+				}
 
+				if (new_family) {
+					Familia f = new Familia(p, pos_in, pos_fin);
+					families.add(f);
+					System.out.println("\n" + name + " adicionado(a) com sucesso à familia " + pos_in + "-" + pos_fin);
+
+				}
 			}
+		} else {
+			System.err.println("\nSecond number is lower than the first or the first number is equal or lower than zero!");
 		}
 	}
 
@@ -167,19 +169,21 @@ public class Main {
 						families.remove(f);
 					}
 				}
-				System.out.println("\n" + name + " removido(a) com sucesso à familia " + f.getPos_in() + "-" + f.getPos_fin());
+				System.out.println(
+						"\n" + name + " removido(a) com sucesso à familia " + f.getPos_in() + "-" + f.getPos_fin());
 				return true;
 
 			}
 		}
 
-		if (!found){
+		if (!found) {
 			System.err.println("\nMembro não existe!");
 		}
 		return false;
 	}
 
-	// FUNÇAO QUE APRESENTA AS POSIÇÕES DOS TERRENOS DE UMA FAMÍLIA, BEM COMO OS SEUS MEMBROS, PROCURANDO PELO NOME DE UMA PESSOA
+	// FUNÇAO QUE APRESENTA AS POSIÇÕES DOS TERRENOS DE UMA FAMÍLIA, BEM COMO OS
+	// SEUS MEMBROS, PROCURANDO PELO NOME DE UMA PESSOA
 	private static void lookup(String name) {
 		System.out.println("\nLookup do membro " + name);
 		boolean found = false;
@@ -222,32 +226,32 @@ public class Main {
 
 	private static void map() {
 		if (!families.isEmpty()) {
-			int counter=0; // VARIÁVEL QUE CONTA O NUMERO DE FAMÍLIAS
+			int counter = 0; // VARIÁVEL QUE CONTA O NUMERO DE FAMÍLIAS
 			int[] values = maxNamePos();
-			System.out.printf("%6s%" + (values[0]+1) + "s", "FAMILY", "NAME");
-			for(int i = values[1]; i<= values[2]; i++) {
+			System.out.printf("%6s%" + (values[0] + 1) + "s", "FAMILY", "NAME");
+			for (int i = values[1]; i <= values[2]; i++) {
 				System.out.printf("%4d", i);
 			}
 			System.out.printf("\n");
 
 			Collections.sort(families, Collections.reverseOrder());
-			
-			for(Familia f : families){
+
+			for (Familia f : families) {
 				counter++;
-				int countMembros=0; // VARIÁVEL QUE CONTA O NUMERO DE MEMBROS DE UMA FAMÍLIA
-				for(Pessoa p : f.getMembros()){
+				int countMembros = 0; // VARIÁVEL QUE CONTA O NUMERO DE MEMBROS DE UMA FAMÍLIA
+				for (Pessoa p : f.getMembros()) {
 					countMembros++;
-					System.out.printf("%6d%" + (values[0]+1) + "s", counter, p.getName());
-					for(int i=values[1]; i<=values[2]; i++){
-						if(i>=f.getPos_in() && i<=f.getPos_fin()){
+					System.out.printf("%6d%" + (values[0] + 1) + "s", counter, p.getName());
+					for (int i = values[1]; i <= values[2]; i++) {
+						if (i >= f.getPos_in() && i <= f.getPos_fin()) {
 							System.out.printf("%4s", "X");
-						}else{
+						} else {
 							System.out.printf("%4s", "");
 						}
 					}
 
-					//IMPEDIR QUE SEJAM COLOCADAS LINHAS (EM BRANCO) A MAIS
-					if(countMembros!=f.countMembros()){
+					// IMPEDIR QUE SEJAM COLOCADAS LINHAS (EM BRANCO) A MAIS
+					if (countMembros != f.countMembros()) {
 						System.out.printf("\n");
 					}
 				}
@@ -262,15 +266,15 @@ public class Main {
 	// FUNÇÃO QUE RETORNA O MAIOR TAMANHO ENCONTRADO NO NOME DE UMA PESSOA
 	private static int[] maxNamePos() {
 		int[] values = new int[3];
-		values[0]=0; // TAMANHO MAXIMO DE UM NOME
-		values[1]=Integer.MAX_VALUE; // POSIÇÃO INICIAL
-		values[2]=0; // POSIÇÃO FINAL
+		values[0] = 0; // TAMANHO MAXIMO DE UM NOME
+		values[1] = Integer.MAX_VALUE; // POSIÇÃO INICIAL
+		values[2] = 0; // POSIÇÃO FINAL
 		for (Familia f : families) {
-			if(f.getPos_in()<values[1]) {
-				values[1]=f.getPos_in();
+			if (f.getPos_in() < values[1]) {
+				values[1] = f.getPos_in();
 			}
-			if(f.getPos_fin()> values[2]) {
-				values[2]=f.getPos_fin();
+			if (f.getPos_fin() > values[2]) {
+				values[2] = f.getPos_fin();
 			}
 			for (Pessoa p : f.getMembros()) {
 				if (values[0] < p.getName().length()) {
