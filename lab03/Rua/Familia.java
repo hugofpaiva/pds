f23433f35117454961a29da4ef82c2376a87b142
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2020-03-11 00:32:13 +0000

    Familia e pessoa

--- lab03/Rua/Familia.java
+++ lab03/Rua/Familia.java
@@ -1,5 +1,35 @@
-package lab03.Rua;
+package LAB3.Rua;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Familia {
-
+	private ArrayList<Pessoa> membros;
+	private ArrayList<Integer> posicoes;
+	public Familia(Pessoa pes, Integer[] pos){
+		membros.add(pes);
+		posicoes.addAll(Arrays.asList(pos));
+	}
+	
+	public void addMembro(Pessoa m) {
+		this.membros.add(m);
+	}
+	
+	public ArrayList<Pessoa> getMembros() {
+		return this.membros;
+	}
+	
+	public ArrayList<Integer> getPosicoes() {
+		return this.posicoes;
+	}
+	
+	public boolean searchIn(Pessoa p) {
+		if(membros.contains(p)) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
+	
 }
--- lab03/Rua/Pessoa.java
+++ lab03/Rua/Pessoa.java
@@ -1,5 +1,39 @@
-package lab03.Rua;
+package LAB3.Rua;
 
-public class Pessoa {
-
+public class Pessoa{
+	private String nome;
+	
+	public Pessoa(String nome) {
+		if(checkName(nome)) {
+			this.nome=nome;
+		}
+	}
+	
+	public boolean checkName(String nome) {
+		if(nome.length() < 3 || nome.length() > 30) {
+			System.out.println("O nome não pode exceder os limites 3 e 30.");
+			return false;
+		}
+		
+		String ultimoChar = String.valueOf(nome.charAt(nome.length()-1));
+		if(!ultimoChar.matches("[^A-Za-z0-9]")){
+			System.out.println("O ultimo caracter tem não pode ser um símbolo");
+			return false;
+		}
+		
+		String primeiroChar = String.valueOf(nome.charAt(0));
+		if(!primeiroChar.matches("[^A-Za-z]")){
+			System.out.println("O primeiro caracter tem de ser uma letra");
+			return false;
+		}
+		
+		if(nome.matches("[A-Za-z0-9._@]+")){
+			System.out.println("O nome apenas permite caracteres alfanuméricos, pontos, underscores e arrobas.");
+			return false;
+		}
+		
+		return true;
+	}
 }
+
+
