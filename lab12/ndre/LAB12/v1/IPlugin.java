4d7ee8bdd5ee088bd2d881c8b6fdc94c26a8ad7c
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2020-05-27 21:30:12 +0100

    d

---
+++ lab12/ndre/LAB12/v1/IPlugin.java
@@ -0,0 +1,5 @@
+package LAB12.v1;
+
+public interface IPlugin {
+	public void fazQualQuerCoisa();
+}
---
+++ lab12/ndre/LAB12/v1/PluginHugo.java
@@ -0,0 +1,14 @@
+package LAB12.v1;
+
+public class PluginHugo implements IPlugin{
+
+	public PluginHugo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void fazQualQuerCoisa() {
+		System.out.println("O hugo é gæ");
+	}
+
+}
